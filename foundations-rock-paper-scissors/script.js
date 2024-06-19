fefdd8ad5a383cf6076fa5512d001223fdd0d967
adrianjosh <62798881+adrianjosh@users.noreply.github.com>
2024-06-19 23:51:26 +0800

    refactored redundant code and added a confirmation at the start of the game

--- foundations-rock-paper-scissors/script.js
+++ foundations-rock-paper-scissors/script.js
@@ -7,28 +7,25 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    let userInput = prompt("are you rock? paper? or scissors?");
-
-    if (userInput === choices[0]
-        || userInput === choices[1]
-        || userInput === choices[2]) {
-
-        return userInput.toLowerCase();
+    let userInput = prompt("Are you rock? paper? or scissors?");
+    for(i = 0; i < choices.length; i++) { 
+        if (userInput === choices[i]) {
+            return userInput.toLowerCase();
+        }
     }
 }
 
 function playRound(humanChoice, computerChoice) {
-    console.log(`u chose ${humanChoice}`);
-    console.log(`computer chose ${computerChoice}`);
-    if((humanChoice === "rock" && computerChoice === "paper")
-        || (humanChoice === "paper" && computerChoice === "scissors")
-        || (humanChoice === "scissors" && computerChoice === "rock")) {
-
+    console.log(`You chose ${humanChoice}`);
+    console.log(`Computer chose ${computerChoice}`);
+    if( (humanChoice === "rock" && computerChoice === "paper") ||
+        (humanChoice === "paper" && computerChoice === "scissors") ||
+        (humanChoice === "scissors" && computerChoice === "rock")
+    ) {
         alert("You Lost!");
         ++computerScore;
     } else if (humanChoice === computerChoice) {
         alert("It's a tie!");
-        
     } else if (humanChoice === undefined) {
         alert("That's an invalid choice!");
     } else {
@@ -41,26 +38,29 @@ function playGame() {
     humanScore = 0;
     computerScore = 0;
 
-    alert("Let's play Rock, Paper, Scissors!");
+    let startMessage = confirm("Let's play Rock, Paper, Scissors?");
+    if (startMessage) {
+        alert("Alright here we go!");
+    } else {
+        alert("You cancelled the operation");
+        return;
+    }
     
     gameRunning = true;
     while(gameRunning) {
-        console.log(`Your score = ${humanScore}, Computer score = ${computerScore}`);
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
 
+        playRound(humanSelection, computerSelection);
+
+        console.log(`Your score = ${humanScore}, Computer score = ${computerScore}`);
         if(humanScore === 5) {
             alert("You won the game!!");
             gameRunning = false;
-            break;
         } else if (computerScore === 5) {
             alert("You lost the game!!");
             gameRunning = false;
-            break;
         }
-
-        let humanSelection = getHumanChoice();
-        let computerSelection = getComputerChoice();
-
-        playRound(humanSelection, computerSelection);
     }
 }
 
