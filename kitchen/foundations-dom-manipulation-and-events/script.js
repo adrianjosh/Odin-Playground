5f87eb4d00818359a26c6f6b7e75137a80f67760
adrianjosh <62798881+adrianjosh@users.noreply.github.com>
2024-06-22 15:07:10 +0800

    finished more example of dom manipulation

--- kitchen/foundations-dom-manipulation-and-events/script.js
+++ kitchen/foundations-dom-manipulation-and-events/script.js
@@ -27,3 +27,20 @@ div.appendChild(p1);
 
 container.appendChild(div);
 
+const btn = document.querySelector("#btn");
+
+btn.addEventListener("click", function (e) {
+    console.log(e.target);
+    e.target.style.background = "blue";
+});
+
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll("button");
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+  // and for each one we add a 'click' listener
+  button.addEventListener("click", () => {
+    alert(button.id);
+  });
+});
