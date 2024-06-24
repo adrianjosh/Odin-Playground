6aee0fbb77c0dbb7423b7d076021fa33ef519da4
adrianjosh <62798881+adrianjosh@users.noreply.github.com>
2024-06-24 16:53:08 +0800

    refactored the code for displaying results in the ui instead of using console.log

--- foundations-rock-paper-scissors/script.js
+++ foundations-rock-paper-scissors/script.js
@@ -1,39 +1,89 @@
 const choices = ["rock","paper","scissors"];
 let humanScore = 0;
 let computerScore = 0;
+let playerChoice;
 
-let userInput;
+let score = document.querySelector(".score");
+let message = document.querySelector(".announcer");
+let playerChoiceImg = document.querySelector("#player-choice");
+let computerChoiceImg = document.querySelector("#computer-choice");
+let btn1 = document.querySelector("#button-container");
+let reset = document.querySelector(".reset-button");
+
+reset.addEventListener("click", resetGame);
+
+btn1.addEventListener("click", (event) =>{
+    let target = event.target;
+    if (humanScore < 5 && computerScore < 5 ){
+        switch(target.id) {
+            case 'rock':
+                playerChoice = target.id;
+                playGame();
+                break;
+            case 'paper':
+                playerChoice = target.id;
+                playGame();
+                break;
+            case 'scissors':
+                playerChoice = target.id;
+                playGame();
+                break;
+        }
+    
+    }
+});
 
 function getComputerChoice() {
-    return choices[Math.floor(Math.random() * choices.length)];
+    let computerChoice = choices[Math.floor(Math.random() * choices.length)];
+
+    if (computerChoice === "rock"){
+        computerChoiceImg.src = "resources/Rock(flipped).png";
+    } else if (computerChoice === "paper"){
+        computerChoiceImg.src = "resources/Paper(flipped).png";
+    } else {
+        computerChoiceImg.src = "resources/Scissor(scissor).png";
+    }
+    return computerChoice;
 }
 
 function getHumanChoice() {
-    for(i = 0; i < choices.length; i++) { 
-        if (userInput === choices[i]) {
-            console.log(`Picked ${userInput}`);
-            return userInput.toLowerCase();
-        }
+    if (playerChoice === "rock"){
+        playerChoiceImg.src = "resources/Rock.png";
+    } else if (playerChoice === "paper"){
+        playerChoiceImg.src = "resources/Paper.png";
+    } else {
+        playerChoiceImg.src = "resources/Scissor.png";
     }
+    return playerChoice;
 }
 
 function playRound(humanChoice, computerChoice) {
-    console.log(`You chose ${humanChoice}`);
-    console.log(`Computer chose ${computerChoice}`);
     if( (humanChoice === "rock" && computerChoice === "paper") ||
         (humanChoice === "paper" && computerChoice === "scissors") ||
         (humanChoice === "scissors" && computerChoice === "rock")
     ) {
-        alert("You Lost!");
+        message.textContent = "YOU LOST! :(";
         ++computerScore;
     } else if (humanChoice === computerChoice) {
-        alert("It's a tie!");
+        message.textContent = "IT'S A TIE :/ ";
     } else if (humanChoice === undefined) {
         alert("That's an invalid choice!");
     } else {
-        alert("You won!");
+        message.textContent = "YOU WON! :)";
         ++humanScore;
     }
+
+    displayScore();
+
+    if(humanScore === 5) {
+        message.textContent = "YOU JUST WON THE GAME!!!";
+    } else if (computerScore === 5) {
+        message.textContent = "YOU LOST THE GAME!!!";
+    }
+}
+
+function displayScore() {
+    score.textContent = `${humanScore} : ${computerScore}`;
 }
 
 function playGame() {
@@ -41,42 +91,13 @@ function playGame() {
     const computerSelection = getComputerChoice();
 
     playRound(humanSelection, computerSelection);
-
-    console.log(`Your score = ${humanScore}, Computer score = ${computerScore}`);
-    if(humanScore >= 5) {
-        alert("You won the game!!");
-        resetGame();
-    } else if (computerScore >= 5) {
-        alert("You lost the game!!");
-        resetGame();
-    }
 }
 
 function resetGame() {
-    alert("Play Again!");
-
     humanScore = 0;
     computerScore = 0;
+    message.textContent = "LET'S PLAY!";
+    computerChoiceImg.src = "resources/question-mark.svg";
+    playerChoiceImg.src = "resources/question-mark.svg";
+    displayScore();
 }
-
-let btn1 = document.querySelector("#button-container");
-btn1.addEventListener("click", (event) =>{
-    let target = event.target;
-
-    switch(target.id) {
-        case 'rock':
-            userInput = target.id;
-            playGame();
-            break;
-        case 'paper':
-            userInput = target.id;
-            playGame();
-            break;
-        case 'scissors':
-            userInput = target.id;
-            playGame();
-            break;
-    }
-    
-
-});
